dfa006ead38b49f77a8a2e008ea68d66d85ead1c
lzc <342502153@qq.com>
2017-01-04 21:38:12 +0800

    create proj ...

---
+++ picer/picer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  picer
+//
+//  Created by Lees on 2017/1/4.
+//  Copyright © 2017年 Lees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
